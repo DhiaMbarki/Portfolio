0e10a2d6609147b049eacc4ec8fb567835002352
Dhia Mbarki <tahridhia22@gmail.com>
2022-10-21 21:45:58 +0100

    fixing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
 import Portfolio from './components/Portfolio'
 import './App.scss'
-import Logo from './components/Home/images/Logo'
+import Logo from './components/Sidebar/Logo'
 
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Logo />} />  
+      <Route path="/Logo" element={<Logo />} />  
     
         <Route path="/" element={<Layout />}>
           <Route path="/Home" element={<Home />} />
@@ -21,6 +22,7 @@ function App() {
           <Route path="/portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
+      </BrowserRouter>
     </>
   )
 }
