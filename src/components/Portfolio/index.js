48c18bde1d894903f9e6f1144fd411c41bca6feb
Dhia Mbarki <tahridhia22@gmail.com>
2022-11-12 01:06:32 +0100

    add card

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,23 +1,11 @@
-import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
-import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import Card from "../Card";
 
+import { properties } from "../constants/data";
 
 const Portfolio = () => { 
-    const [letterClass, setLetterClass] = useState('text-animate');
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
-
-        return () => {
-            clearTimeout(timer);
-        }
-    });
-
+   
    
 
  
@@ -26,45 +14,16 @@ const Portfolio = () => {
 
 
     return (
-        <>
-            <div className="container portfolio-page">
-                <h1 className="page-title">
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={"Portfolio".split("")}
-                        idx={15}
-                    />
-                </h1>
+        <div className="">
+        <div className="properties">
+          {properties.map((item) => (
+            <Card data={item} key={item.id} />
+          ))}
+        </div>
+        <Loader type="pacman" />
 
-
-
-
-
-                
-            <div className=" textt ">
-
-                <div className="max-w-5x1">
-
-            <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-    </div>
-            </div>
-            </div>
-            
-            
-
-            <Loader type="pacman" />
-        </>
+      </div>
+      
     );
 }
 
