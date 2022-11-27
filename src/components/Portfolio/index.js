a1029ef3fe38299e56b2b287fc66ae2a1e9a3f96
Dhia Mbarki <tahridhia22@gmail.com>
2022-11-27 11:52:19 +0100

    updated

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,89 +1,68 @@
+import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
-import  "./index.scss";
-import picture from "./picture.png"
-import picture2 from "./picture2.png"
-import picture3 from "./picture3.jpg"
-
-
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
 
 const Portfolio = () => { 
-   
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const [portfolio, setPortfolio] = useState([]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    });
+
    
 
  
 
     const renderPortfolio = (portfolio) => {
         return (
-          <div>
-         
-       </div>
+            <div className="images-container">
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className="image-box" key={idx}>
+                                <img 
+                                src={port.image}
+                                className="portfolio-image"
+                                alt="portfolio" />
+                                <div className="content">
+                                    <p className="title">{port.name}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         );
     }
 
 
     return (
         <>
-        <div>
-           <div className="card">
-          <div >
-             <h1>Apps</h1>
-             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue.
-             </p>
-             <button className="sh_btn">Read more</button>
-          </div>
-          <div className="cover">
-             <div className="coverFront">
-                <div>
-                   <h5>Apps</h5>
-                   <img src={picture}  />
-                </div>
-             </div>
-             <div className="coverBack"></div>
-          </div>
-       </div>
-    
-       <div className="card">
-          <div className="test1" >
-             <h1>Marketing</h1>
-             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue.
-             </p>
-             <button className="sh_btn">Read more</button>
-          </div>
-          <div className="cover">
-             <div className="coverFront">
-                <div>
-                   <h5>Marketing</h5>
-                   {/* <img src="img2.svg" alt="" className="sh_img"> */}
-                </div>
-             </div>
-             <div className="coverBack"></div>
-          </div>
-       </div>
-    
-       <div className="card">
-          <div>
-             <h1>Security</h1>
-             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices in iaculis nunc sed augue.
-             </p>
-             <button className="sh_btn">Read more</button>
-          </div>
-          <div className="cover">
-             <div className="coverFront">
-                <div>
-                   <h5>Security</h5>
-                   {/* <img src="img3.svg" alt="" className="sh_img"> */}
-                </div>
-             </div>
-             <div className="coverBack"></div>
-          </div>
-       </div>
-            <Loader type="pacman" />
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={"Portfolio".split("")}
+                        idx={15}
+                    />
+                </h1>
+                <div>{renderPortfolio(portfolio)}</div>
             </div>
+            <Loader type="pacman" />
         </>
-
     );
 }
 
