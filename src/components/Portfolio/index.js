03134910990bb3e9c37a3d840bf273e7dfe2699a
DhiaMbarki <tahridhia22@gmail.com>
2022-11-29 21:51:52 +0100

    fix

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,7 +5,9 @@ import project1 from "./imgs/pic1.png";
 import project2 from "./imgs/pic2.jpg";
 import project3 from "./imgs/pic3.png";
 import project4 from "./imgs/pic4.jpg";
-// import project5 from "./imgs/";
+
+import MyImage from './imgs/left.gif';
+
 
 
 const Portfolio = () => { 
@@ -14,23 +16,24 @@ const Portfolio = () => {
         {
         //   title: "Nature Is Life",
           image: project1,
+
         //   description:
         //     "Praesent quis congue nisi. Vivamus dapibus suscipit magna at imperdiet. Mauris consectetur pharetra metus eu suscipit. Maecenas augue tortor, consequat vitae lacus id, pharetra tempor mauris. Suspendisse sodales commodo erat, non imperdiet nisl scelerisque at. Nulla porttitor gravida diam, in ornare ipsum accumsan bibendum. Morbi ut ante metus. "
         },
         {
-          title: "Into The Waters",
+          // title: "Into The Waters",
           image: project3,
           // description:
             // "Duis at tellus vitae velit aliquet varius. Fusce luctus libero et ligula tristique lobortis. Vestibulum eu placerat risus, eu semper augue. Integer at purus sit amet elit pretium viverra. Suspendisse id fringilla nibh, nec dictum urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
         },
         {
-          title: "Miniature Wonderers",
+          // title: "Miniature Wonderers",
           image: project4,
           // description:
             // "Praesent quis congue nisi. Vivamus dapibus suscipit magna at imperdiet. Mauris consectetur pharetra metus eu suscipit. Maecenas augue tortor, consequat vitae lacus id, pharetra tempor mauris. Suspendisse sodales commodo erat, non imperdiet nisl scelerisque at. Nulla porttitor gravida diam, in ornare ipsum accumsan bibendum. Morbi ut ante metus. "
         },
         {
-          title: "Higher Than The Stars",
+          // title: "Higher Than The Stars",
         //   image: project5,
           // description:
             // "Praesent quis congue nisi. Vivamus dapibus suscipit magna at imperdiet. Maecenas augue tortor, consequat vitae lacus id, pharetra tempor mauris. Suspendisse sodales commodo erat, non imperdiet nisl scelerisque at. Nulla porttitor gravida diam, in ornare ipsum accumsan bibendum. Morbi ut ante metus. Proin rhoncus magna lectus, nec consequat augue ultricies eu."
@@ -60,11 +63,21 @@ const Portfolio = () => {
 
     return (
         <>
+          <div className="move">
+        <img src={project1}   />
+
+        </div>
          <div className="all">
+    
       <Slider sliderSettings={sliderSettings} />
+      
+
     </div>
-            
+    
+
+    
             <Loader type="pacman" />
+           
         </>
     );
 }
